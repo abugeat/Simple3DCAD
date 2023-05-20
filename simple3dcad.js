bf2c44b0fe9ef999aa71ea320a14fdcba394652a
abugeat <45351626+abugeat@users.noreply.github.com>
2023-05-20 20:12:04 +0200

    hover color effect done

--- simple3dcad.js
+++ simple3dcad.js
@@ -1,3 +1,164 @@
-console.log("hello world");
-console.log("hello world again");
 
+import * as dat from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import * as THREE from 'three';
+import {
+	acceleratedRaycast, computeBoundsTree, disposeBoundsTree,
+	SAH,
+} from 'three-mesh-bvh';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils.js";
+
+THREE.Mesh.prototype.raycast = acceleratedRaycast;
+THREE.BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
+THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
+
+// Global variables
+let renderer, scene, camera, controls;
+let mesh, geometry, material, containerObj;
+let selectedObject = null;
+// geometry = new THREE.BufferGeometry();
+geometry = new THREE.SphereGeometry(1);
+
+const pointer = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+raycaster.firstHitOnly = true;
+
+// const materialhit = new THREE.MeshBasicMaterial(
+//     { color: 0xff6347, transparent: true, opacity:0.5 }
+// );
+// const materialrays = new THREE.MeshBasicMaterial(
+//     { color: 0xff6347, transparent: true, opacity:0.5 }
+// );
+
+init();
+addCube();
+
+function init() {
+
+	// renderer setup
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setClearColor( 0x000000, 1 );
+	document.body.appendChild( renderer.domElement );
+
+	// scene setup
+	scene = new THREE.Scene();
+
+    // axes helper
+    const axesHelper = new THREE.AxesHelper(1); // 100 est la taille des axes
+    scene.add(axesHelper);
+
+    // ambient light
+	const light = new THREE.DirectionalLight( 0xffffff, 1.0 );
+	light.position.set( 1, 2, 1 );
+	scene.add( light );
+	const light2 = new THREE.DirectionalLight( 0xffffff, 0.75 );
+	light2.position.set( -1, 0.5, -1 );
+	scene.add( light2 );
+	// scene.add( new THREE.AmbientLight( 0xffffff, 0.5 ) );
+	scene.background = new THREE.Color( 0xffffff );
+
+
+	// geometry setup
+	containerObj = new THREE.Object3D();
+	material = new THREE.MeshPhongMaterial( { color: 0x999999 , side: THREE.DoubleSide} );
+	scene.add( containerObj );
+
+	// camera setup
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 50 );
+	// camera.position.set(0, 10, 10) ;
+	camera.far = 100000;
+	camera.updateProjectionMatrix();
+    camera.position.set( 1, 2, 3);
+
+	// control setup
+	controls = new OrbitControls( camera, renderer.domElement );
+	// controls.target.set( 25, 0, -25 );
+	controls.target.set(0, 0, 0);
+	controls.update();
+	controls.addEventListener('change', function(){
+		renderer.render( scene, camera );
+	});
+
+    // lil-gui setup
+    const gui = new dat.GUI();
+    gui.title("Simple3DCAD");
+    
+    // resize eventlistener
+	window.addEventListener( 'resize', function () {
+				
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	
+		renderer.render( scene, camera );
+	
+	}, false );
+
+    document.addEventListener( 'pointermove', onPointerMove );
+}
+
+function addCube() {
+    let geometry_cube = new THREE.BoxGeometry( 0.5, 1, 1.5 );
+    let mesh_cube = new THREE.Mesh( geometry_cube, material );
+    mesh_cube.position.set(0, 0, 0);
+    geometry_cube
+    containerObj.add( mesh_cube );
+
+    geometry = BufferGeometryUtils.mergeGeometries([geometry, geometry_cube]);
+
+    renderer.render( scene, camera );
+
+    letcomputeBoundsTree();
+}
+
+function letcomputeBoundsTree() {
+	console.time( 'computing bounds tree' );
+	geometry.computeBoundsTree( {
+		// maxLeafTris: 5,
+		strategy: parseFloat( SAH ),
+	});
+	geometry.boundsTree.splitStrategy = SAH;
+	console.timeEnd( 'computing bounds tree' );
+}
+
+function onPointerMove( event ) {
+
+    if ( selectedObject ) {
+
+        selectedObject.material.color.set( '#69f' );
+        selectedObject = null;
+
+    }
+
+    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera( pointer, camera );
+
+    const intersects = raycaster.intersectObject( containerObj, true );
+
+    if ( intersects.length > 0 ) {
+
+        const res = intersects.filter( function ( res ) {
+
+            return res && res.object;
+
+        } )[ 0 ];
+
+        if ( res && res.object ) {
+
+            selectedObject = res.object;
+            console.log(selectedObject);
+            selectedObject.material.color.set( '#f00' );
+
+        }
+
+    }
+
+    renderer.render( scene, camera );
+
+}
